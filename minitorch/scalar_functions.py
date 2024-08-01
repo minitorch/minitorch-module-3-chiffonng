e9811ad9bc4b47006c0294dc8865f0bf006bee3a
My (Chiffon) Nguyen <s-tngu18@haw-landshut.de>
2024-08-01 22:57:19 +0200

    chore: Add scalar_functions.py module to use with scalar.py module

---
+++ minitorch/scalar_functions.py
@@ -0,0 +1,212 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import minitorch
+
+from . import operators
+from .autodiff import Context
+
+if TYPE_CHECKING:
+    from typing import Tuple
+
+    from .scalar import Scalar, ScalarLike
+
+
+def wrap_tuple(x):  # type: ignore
+    "Turn a possible value into a tuple"
+    if isinstance(x, tuple):
+        return x
+    return (x,)
+
+
+def unwrap_tuple(x):  # type: ignore
+    "Turn a singleton tuple into a value"
+    if len(x) == 1:
+        return x[0]
+    return x
+
+
+class ScalarFunction:
+    """
+    A wrapper for a mathematical function that processes and produces
+    Scalar variables.
+
+    This is a static class and is never instantiated. We use `class`
+    here to group together the `forward` and `backward` code.
+    """
+
+    @classmethod
+    def _backward(cls, ctx: Context, d_out: float) -> Tuple[float, ...]:
+        return wrap_tuple(cls.backward(ctx, d_out))  # type: ignore
+
+    @classmethod
+    def _forward(cls, ctx: Context, *inps: float) -> float:
+        return cls.forward(ctx, *inps)  # type: ignore
+
+    @classmethod
+    def apply(cls, *vals: "ScalarLike") -> Scalar:
+        raw_vals = []
+        scalars = []
+        for v in vals:
+            if isinstance(v, minitorch.scalar.Scalar):
+                scalars.append(v)
+                raw_vals.append(v.data)
+            else:
+                scalars.append(minitorch.scalar.Scalar(v))
+
+                # Fix back=None
+                raw_vals.append(float(v))
+
+        # Create the context.
+        ctx = Context(False)
+
+        # Call forward with the variables.
+        c = cls._forward(ctx, *raw_vals)
+        assert isinstance(c, float), "Expected return type float got %s" % (type(c))
+
+        # Create a new variable from the result with a new history.
+        back = minitorch.scalar.ScalarHistory(cls, ctx, scalars)
+        return minitorch.scalar.Scalar(c, back)
+
+
+# Examples
+class Add(ScalarFunction):
+    "Addition function $f(x, y) = x + y$"
+
+    @staticmethod
+    def forward(ctx: Context, a: float, b: float) -> float:
+        return a + b
+
+    @staticmethod
+    def backward(ctx: Context, d_output: float) -> Tuple[float, ...]:
+        return d_output, d_output
+
+
+class Log(ScalarFunction):
+    "Log function $f(x) = log(x)$"
+
+    @staticmethod
+    def forward(ctx: Context, a: float) -> float:
+        ctx.save_for_backward(a)
+        return operators.log(a)
+
+    @staticmethod
+    def backward(ctx: Context, d_output: float) -> float:
+        (a,) = ctx.saved_values
+        return operators.log_back(a, d_output)
+
+
+class Mul(ScalarFunction):
+    "Multiplication function $f(x, y) = x * y$"
+
+    @staticmethod
+    def forward(ctx: Context, a: float, b: float) -> float:
+        ctx.save_for_backward(a, b)
+        return a * b
+
+    @staticmethod
+    def backward(ctx: Context, d_output: float) -> Tuple[float, float]:
+        """The derivative of a * b with respect to a and b is b and a, multiplied by the output derivative."""
+        (a, b) = ctx.saved_values
+        return operators.mul(b, d_output), operators.mul(a, d_output)
+
+
+class Inv(ScalarFunction):
+    "Inverse function $f(x) = 1/x$"
+
+    @staticmethod
+    def forward(ctx: Context, a: float) -> float:
+        ctx.save_for_backward(a)
+        return operators.inv(a)
+
+    @staticmethod
+    def backward(ctx: Context, d_output: float) -> float:
+        """Derivative of 1/x is -1/x^2"""
+        (a,) = ctx.saved_values
+        return operators.inv_back(a, d_output)
+
+
+class Neg(ScalarFunction):
+    "Negation function $f(x) = -x$"
+
+    @staticmethod
+    def forward(ctx: Context, a: float) -> float:
+        return operators.neg(a)
+
+    @staticmethod
+    def backward(ctx: Context, d_output: float) -> float:
+        """Derivative of -x is -1, multiplied by d_output."""
+        return operators.neg(d_output)
+
+
+class Sigmoid(ScalarFunction):
+    "Sigmoid function $f(x) = 1.0 / (1.0 + e^{-x})$"
+
+    @staticmethod
+    def forward(ctx: Context, a: float) -> float:
+        sig = operators.sigmoid(a)
+        ctx.save_for_backward(sig)
+        return sig
+
+    @staticmethod
+    def backward(ctx: Context, d_output: float) -> float:
+        """Derivative of sigmoid is sigmoid * (1 - sigmoid)"""
+        (sig,) = ctx.saved_values
+        return operators.mul(sig, 1.0 - sig) * d_output
+
+
+class ReLU(ScalarFunction):
+    "ReLU function $f(x) = x$ if x is greater than 0, else 0"
+
+    @staticmethod
+    def forward(ctx: Context, a: float) -> float:
+        ctx.save_for_backward(a)
+        return operators.relu(a)
+
+    @staticmethod
+    def backward(ctx: Context, d_output: float) -> float:
+        """Derivative of ReLU is 1 if x > 0, else 0"""
+        (a,) = ctx.saved_values
+        return operators.relu_back(a, d_output)
+
+
+class Exp(ScalarFunction):
+    "Exp function"
+
+    @staticmethod
+    def forward(ctx: Context, a: float) -> float:
+        exp = operators.exp(a)
+        ctx.save_for_backward(exp)
+        return exp
+
+    @staticmethod
+    def backward(ctx: Context, d_output: float) -> float:
+        """Derivative of exp is exp, multiplied by d_output."""
+        (exp,) = ctx.saved_values
+        return operators.mul(exp, d_output)
+
+
+class LT(ScalarFunction):
+    "Less-than function $f(x) =$ 1.0 if x is less than y else 0.0"
+
+    @staticmethod
+    def forward(ctx: Context, a: float, b: float) -> float:
+        return operators.lt(a, b)
+
+    @staticmethod
+    def backward(ctx: Context, d_output: float) -> Tuple[float, float]:
+        """Derivative of less-than (1.0 or 0.0) is 0"""
+        return 0.0, 0.0
+
+
+class EQ(ScalarFunction):
+    "Equal function $f(x) =$ 1.0 if x is equal to y else 0.0"
+
+    @staticmethod
+    def forward(ctx: Context, a: float, b: float) -> float:
+        return operators.eq(a, b)
+
+    @staticmethod
+    def backward(ctx: Context, d_output: float) -> Tuple[float, float]:
+        return 0.0, 0.0
